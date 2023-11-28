974ba70ec373709d3b111f4184c8aa6413f1716c
sachinnsinghh <sachinsingh000094gmail.com>
2023-11-28 15:16:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useState , useEffect} from 'react';
 import './App.css';
+import {getPosts , getRandomUser} from "./API";
+import PostCard from './components/PostCard';
+import UserCard from './components/UserCard';
 
 function App() {
+  const [data , setData ] = useState(null);
+  const [userData , setUserData] = useState(null);
+
+  useEffect(()=>{
+    getPosts().then((posts)=> setData(posts));
+  }, []);
+  useEffect(()=>{
+    getRandomUser().then((user)=> setUserData (user.results[0]));
+  }, []);
+  const refresh =() => {
+    getRandomUser().then((user)=> setUserData (user.results[0]));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { userData && <UserCard data ={userData} />}
+      <button onClick={refresh}>Refresh user</button>
+      {
+        data ? data.map((e)=> <PostCard key={e.id} title={e.title} body= {e.body} />) : <p> NO DATA</p>
+      }
     </div>
   );
 }
